1ae2bea51c917c8e7c0cd226e8711587074b2d8d
Tanmay Elinje <tanmayelinje@gmail.com>
2025-09-05 23:15:01 +0530

    Prepare for Render deployment

--- backend/urls.py
+++ backend/urls.py
@@ -8,8 +8,22 @@ from rest_framework_simplejwt.views import (
 )
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({
+        "message": "InventoryPro API is running 🚀",
+        "endpoints": {
+            "admin": "/admin/",
+            "api": "/api/",
+            "auth_token": "/api/token/"
+        }
+    })
+
 
 urlpatterns = [
+    path('', home, name='home'),  # 👈 root route add kiya
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
