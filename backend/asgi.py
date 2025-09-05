89461bc018ce4a5c20230f790ffb2a3d38968182
Tanmay Elinje <tanmayelinje@gmail.com>
2025-09-05 16:52:00 +0530

    Configure static files for production

--- backend/asgi.py
+++ backend/asgi.py
@@ -5,15 +5,15 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from inventory.middleware import TokenAuthMiddleware
-import inventory.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-# --- THIS IS THE FIX ---
-# This line ensures Django is fully initialized before any other imports
-# that might depend on it (like models in your middleware).
+# Initialize Django first
 django.setup()
+
+# Import things that rely on Django apps AFTER setup
+from inventory.middleware import TokenAuthMiddleware
+import inventory.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
