f72e184c36ce99aca5d10a0c77c794c0d0a09b4c
Tanmay Elinje <tanmayelinje@gmail.com>
2025-09-05 18:03:13 +0530

    Configure static files for production

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-# --- ADD THIS LINE ---
-# Load environment variables from the secret file
-source .env
-
-# These lines remain the same
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply migrations
 python manage.py migrate
