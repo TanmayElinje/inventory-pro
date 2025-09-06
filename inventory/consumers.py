1171cf6460f446c4cb12db7e56e08bb2a555f8ca
Tanmay Elinje <tanmayelinje@gmail.com>
2025-09-06 12:33:53 +0530

    Update consumers.py

--- inventory/consumers.py
+++ inventory/consumers.py
@@ -5,7 +5,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ProductConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.group_name = 'products_group'
-        # Join room group
         await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
@@ -13,16 +12,13 @@ class ProductConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
         await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
-    # This function is called when a message is sent from the backend (e.g., from a view)
     async def product_update(self, event):
         product_data = event['product']
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'type': 'product_update',
             'product': product_data
