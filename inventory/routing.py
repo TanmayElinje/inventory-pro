cc3ee74dc5511ce38d43c6fc6d90ea6e0679305f
Tanmay Elinje <tanmayelinje@gmail.com>
2025-09-06 12:35:06 +0530

    Update routing.py

--- inventory/routing.py
+++ inventory/routing.py
@@ -2,8 +2,6 @@
 from django.urls import re_path
 from . import consumers
 
-# The original path might have been too loose.
-# Let's make it more specific.
 websocket_urlpatterns = [
     re_path(r'^ws/products/$', consumers.ProductConsumer.as_asgi()),
 ]
