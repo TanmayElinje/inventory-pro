40a8f464cfb714645a7aa0a3285942da074fb8f3
Tanmay Elinje <tanmayelinje@gmail.com>
2025-09-06 12:34:28 +0530

    Update middleware.py

--- inventory/middleware.py
+++ inventory/middleware.py
@@ -4,10 +4,6 @@ from channels.db import database_sync_to_async
 
 @database_sync_to_async
 def get_user(token_key):
-    """
-    This function is now self-contained and only imports what it needs,
-    when it needs it, preventing startup errors.
-    """
     from rest_framework_simplejwt.tokens import AccessToken
     from django.contrib.auth.models import User
     try:
